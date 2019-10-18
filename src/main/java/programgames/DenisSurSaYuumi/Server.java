c587c4dd7602843bfd24ae0397f87afcd36ac5e0
programgames <julgamer1@gmail.com>
2019-10-18 21:56:22 +0200

    refacto arguments

--- src/main/java/programgames/DenisSurSaYuumi/Server.java
+++ src/main/java/programgames/DenisSurSaYuumi/Server.java
@@ -1,5 +1,7 @@
 package programgames.DenisSurSaYuumi;
 
+import org.apache.commons.cli.*;
+
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.io.BufferedReader;
@@ -14,24 +16,42 @@ public class Server {
     private static Socket clientSocket;
     private static BufferedReader bufferedReader;
     private static Robot robot;
+    private static CommandLine cmd;
+
     public static void main(String[] args) {
 
-        if (args.length == 1) {
+        initArguments(args);
 
-            try {
-                initServer(args[0]);
-            } catch (Exception e) {
-                System.err.println("An error as occured while initializing the server");
-                e.printStackTrace();
-                System.exit(-1);
-            }
-        } else {
-            System.out.println("Please provide the ip of the server as first argument");
-            System.out.println("Please provide the port of the server as second argument");
+        try {
+            initServer();
+        } catch (Exception e) {
+            System.err.println("An error as occured while initializing the server");
+            e.printStackTrace();
             System.exit(-1);
         }
 
         readClient();
+    }
+
+    private static void initArguments(String[] args) {
+
+        Options options = new Options();
+
+        Option output = new Option("p", "port", true, "port of the server");
+        output.setRequired(true);
+        options.addOption(output);
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.out.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+
+            System.exit(1);
+        }
     }
 
     private static void readClient() {
@@ -47,13 +67,9 @@ public class Server {
         try {
 
             while ((inputLine = bufferedReader.readLine()) != null) {
-
-                if (inputLine.equals("R")) {
-
-                        robot.keyPress(KeyEvent.VK_R);
-                        robot.keyRelease(KeyEvent.VK_R);
-                }
+                executeKey(inputLine);
             }
+
 
         } catch (Exception e) {
             System.err.println("An error as occured while reading the client data");
@@ -68,8 +84,17 @@ public class Server {
         System.out.println("close");
     }
 
-    private static void initServer(String port) throws IOException {
-        serverSocket = new ServerSocket(Integer.parseInt(port));
+    private static void executeKey(String inputLine) {
+
+        if(inputLine.equals("A"))
+        {
+            robot.keyPress(KeyEvent.VK_A);
+            robot.keyRelease(KeyEvent.VK_A);
+        }
+    }
+
+    private static void initServer() throws IOException {
+        serverSocket = new ServerSocket(Integer.parseInt(cmd.getOptionValue("ip")));
         clientSocket = serverSocket.accept();
         bufferedReader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
